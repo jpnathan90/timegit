70f16fc1577be968d61f39c15da0818f83bc7350
jpnathan90 <jpnathan90@gmail.com>
2020-04-05 00:21:09 +0530

    test File

---
+++ analytics.js
@@ -0,0 +1,13 @@
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("POST", "https://www.w3schools.com/js/demo_post2.asp", true);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.send("fname=Henry&lname=Ford");
+}
+
+loadDoc();
