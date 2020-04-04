709cd01595c3ff4b3f0016104aef160f3294e3bd
jpnathan90 <jpnathan90@gmail.com>
2020-04-05 00:25:21 +0530

    Create testAnn.js

---
+++ testAnn.js
@@ -0,0 +1,11 @@
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "https://www.w3schools.com/js/demo_get.asp", true);
+  xhttp.send();
+}
+loadDoc();
